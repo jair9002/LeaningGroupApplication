a6cc485ea367042259119b1e9ddd15342e6918a9
jair9002 <cwoh9002@naver.com>
2019-11-18 21:44:13 +0900

    test

--- app/src/main/java/com/example/leaninggroupapplication/LoadingActivity.java
+++ app/src/main/java/com/example/leaninggroupapplication/LoadingActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.os.Handler;
 
 public class LoadingActivity extends AppCompatActivity {
-
+    //dfsdf
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
